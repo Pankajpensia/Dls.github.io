b5345c269051c0461d4d496dad3dc572c007237f
Pankajpensia <110286931+Pankajpensia@users.noreply.github.com>
2023-06-02 06:04:50 -0600

    register.js

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,4 @@
-
+alert("Welcome To DLS");
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
